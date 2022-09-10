f321218fd2573d0cd9f7fd774ad3a41988d4bbaa
Tolemo <jellasonjude@gmail.com>
2022-09-10 04:47:38 -0700

    Hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+*main - print the string in the put function
+*
+*Discription: using the main function 
+*this program prints "Programming is like building a multilingual puzzle
+* Return: o
+*/
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (o);
+}
+^C
+^c
+i
