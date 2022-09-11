95d304148c9ec845901891371b4847c6553c42bf
Tolemo <jellasonjude@gmail.com>
2022-09-11 16:21:21 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
-*main - print the string in the put function
-*
-*Discription: using the main function 
-*this program prints "Programming is like building a multilingual puzzle
-* Return: o
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (o);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
-^C
-^c
-i
