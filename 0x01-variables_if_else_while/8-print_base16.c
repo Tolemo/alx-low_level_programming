878e30a454767ef49bac543a241843bb36ce0e6a
Tolemo <jellasonjude@gmail.com>
2022-09-12 18:25:35 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int d = 48;
+	char c = 'a';
+
+	while
+		(d < 58);
+	{
+		putchar(d);
+		d++;
+	}
+
+	while
+		(c <= 'f');
+		c++;
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
