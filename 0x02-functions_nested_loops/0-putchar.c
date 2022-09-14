bf6a5f5a0f8208d7fe845ae9576464827a1159ac
Tolemo <jellasonjude@gmail.com>
2022-09-14 10:56:11 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
- * main - print puchar
+ * main - print putcher
  * Return: always 0
  */
 
 int main(void)
 {
-	_putchar('_');
+	_putchar('-');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
